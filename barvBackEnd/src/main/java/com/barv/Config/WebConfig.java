81a235154e5f51a069557148b4e1456c25d4ea0b
Rain Rahni <rain.rahni5@gmail.com>
2023-08-22 11:30:09 +0300

    adding github crossorigings

--- barvBackEnd/src/main/java/com/barv/Config/WebConfig.java
+++ barvBackEnd/src/main/java/com/barv/Config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
-                .allowedOrigins("https://rainrahni.github.io/barv")
+                .allowedOrigins("http://127.0.0.1:5500/")
+                .allowedOrigins("https://rainrahni.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
