abf93fdd6ee14704f2e6cbb544497a33a9f72cb6
Rain Rahni <rain.rahni5@gmail.com>
2023-11-01 22:32:03 +0200

    add foodview

---
+++ vue/barv-front/barv/src/views/FoodView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="food">
+    <h1>This is an food page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .food {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup>
+</script>
