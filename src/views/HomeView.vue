2884aa78e7121d26150e08c303b69aa08fcdbfa5
RainRahni <rain.rahni@mail.ee>
2023-11-18 21:28:53 +0200

    14 add main page (#16)
    * add pictures, style them, need more universal/responsive way to fit them
    
    * add pictures, style them, need more universal/responsive way to fit them
    
    * add pictures, style them, need more universal/responsive way to fit them
    
    * add pictures, style them, need more universal/responsive way to fit them
    
    ---------
    
    Co-authored-by: Rain Rahni <rain.rahni5@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import MainPagePicture from "@/components/MainPagePicture.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MainPagePicture id="trainingPic" imageUrl='src/assets/images/trainingpic.jpg' />
+    <MainPagePicture id="foodPic" imageUrl="src/assets/images/foodpic.jpg"/>
   </main>
 </template>
+<style>
+main {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+#trainingPic {
+  margin-left: -26.5%;
+}
+#foodPic {
+  margin-right: -26%;
+}
+</style>
