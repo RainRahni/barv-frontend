e795793f06a2b98ca23c5ac3d0de26b7d3468512
RainRahni <rain.rahni@mail.ee>
2024-01-27 11:46:32 +0200

    add path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/barv-frontend/",
   plugins: [
     vue(),
   ],
@@ -13,4 +14,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+
 })
